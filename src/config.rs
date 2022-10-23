ddd111f21d57ad71050591cf14013925a692c45b
Draculente <me@draculente.eu>
2022-10-23 11:34:40 +0200

    get working version

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,31 @@
 use std::fs;
 
-use serde::Deserialize;
+use serde_derive::Deserialize;
+use urlencoding::encode;
 
-#[derive(Debug, Deserialize)]
-pub struct SearchConfig {
-    pub key: String,
-    pub url: String,
+#[derive(Debug)]
+struct SearchConfig {
+    key: String,
+    url: String,
+    key_with_space: String,
+    index_of_replace: usize,
 }
 
 #[derive(Debug, Deserialize)]
+struct ConfigRedirect {
+    key: String,
+    url: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct RawConfig {
+    sites: Vec<ConfigRedirect>,
+    prefix: char,
+}
+
+#[derive(Debug)]
 pub struct Config {
-    pub sites: Vec<SearchConfig>,
-    pub prefix: char,
+    sites: Vec<SearchConfig>,
 }
 
 impl Config {
@@ -19,19 +33,44 @@ impl Config {
         let content = fs::read_to_string(path)
             .expect(format!("please provide a config file at {}", path).as_str());
 
-        let mut config: Config = toml::from_str(content.as_str())
+        let raw: RawConfig = toml::from_str(content.as_str())
             .expect(format!("error in the config file at {}", path).as_str());
-        //serde_json::from_str(content.as_str()).expect(format!("error in the config file at {}", path).as_str())
 
-        config.sites = config
-            .sites
-            .into_iter()
-            .map(|mut e| {
-                e.key.insert(0, config.prefix);
-                e
+        Config {
+            sites: raw
+                .sites
+                .into_iter()
+                .map(|e| SearchConfig {
+                    key: format!("{}{}", raw.prefix, e.key),
+                    key_with_space: format!("{}{} ", raw.prefix, e.key),
+                    url: e.url.replace("{{s}}", ""),
+                    index_of_replace: e
+                        .url
+                        .find("{{s}}")
+                        .expect("please indicate the position to be replaced in an url with {{s}}"),
+                })
+                .collect(),
+        }
+    }
+
+    pub fn find_redirect(&self, search_string: &str) -> Option<String> {
+        self.sites
+            .iter()
+            .find(|e| search_string.ends_with(&e.key) || search_string.contains(&e.key_with_space))
+            .or_else(|| self.sites.get(0))
+            .map(|e| {
+                let mut redirect = e.url.clone();
+                redirect.insert_str(
+                    e.index_of_replace,
+                    encode(
+                        &search_string
+                            .replace(&e.key_with_space, "")
+                            .replace(&e.key, ""),
+                    )
+                    .into_owned()
+                    .as_str(),
+                );
+                redirect
             })
-            .collect();
-
-        config
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 use std::{
     env,
-    io::Read,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
     sync::Arc,
     thread,
     time::Duration,
 };
 
-mod config;
+pub mod config;
 use config::Config;
 use thhp::{Request, Status::Complete};
+use urlencoding::decode;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn run(config: Arc<Config>) -> Result<()> {
+pub fn run(config: Arc<Config>) -> Result<()> {
     let listener = TcpListener::bind(format!("127.0.0.1:{}", env::var("WEBCOMMAND_PORT")?))?;
 
     // accept connections and process them serially
@@ -21,8 +22,16 @@ fn run(config: Arc<Config>) -> Result<()> {
         let c = Arc::clone(&config);
         let mut s = stream?;
         thread::spawn(move || {
-            handle_client(&mut s, &c);
-            //TODO: Send error to client if handle_client errors
+            if let Err(e) = handle_client(&mut s, &c) {
+                println!("Err {}", e);
+                let _ = s.write_all(
+                    b"HTTP/1.1 400 Invalid Request\r\n\
+                                      Content-Type: text/plain\r\n\
+                                      \r\n",
+                );
+                let _ = s.write_all(e.to_string().as_bytes());
+                let _ = s.write_all(b"\n");
+            }
         });
     }
     Ok(())
@@ -31,29 +40,62 @@ fn run(config: Arc<Config>) -> Result<()> {
 //https://github.com/vi/http-bytes/blob/master/examples/http_proxy.rs
 
 fn handle_client(stream: &mut TcpStream, config: &Config) -> Result<()> {
-    let mut buf = vec![0u8; 1024];
+    let mut buf = vec![0u8; 1024 * 2];
     let mut fill_meter = 0usize;
-    let mut headers = Vec::<thhp::HeaderField>::with_capacity(16);
 
     if let Err(_) = stream.set_read_timeout(Some(Duration::from_millis(1000))) {
         //TODO: Errorhandling;
     }
 
+    if let Err(_) = stream.set_write_timeout(Some(Duration::from_millis(1000))) {
+        //TODO: Errorhandling;
+    }
+
     loop {
-        let read1 = stream.read(&mut buf[fill_meter..])?;
+        fill_meter = fill_meter + stream.read(&mut buf[fill_meter..])?;
 
         {
-            let b = &buf[0..(fill_meter + read1)];
-            match Request::parse(b, &mut headers) {
-                Ok(Complete((ref req, len))) => return handle_request(req, config),
+            let mut headers = Vec::<thhp::HeaderField>::with_capacity(16);
+
+            match Request::parse(&buf[0..fill_meter], &mut headers) {
+                Ok(Complete((ref req, _))) => return handle_request(stream, req, config),
                 Ok(thhp::Incomplete) => (),
                 Err(_) => todo!(),
             }
         }
+
+        if fill_meter > 1024 * 100 {
+            Err("request to big")?;
+        }
+
+        buf.resize(fill_meter + 1024, 0u8);
+
+        thread::sleep(Duration::from_millis(1))
     }
 }
 
-fn handle_request(req: &Request, config: &Config) -> Result<()> {
-    // TODO: logic from old request handler.
-    todo!()
+fn handle_request(stream: &mut TcpStream, req: &Request, config: &Config) -> Result<()> {
+    if req.method != "GET" {
+        Err("method not supported")?
+    };
+
+    // TODO: Better error handling when no prefix is present
+    let redirect = decode(req.target.strip_prefix("/").unwrap_or_default())?;
+
+    match config.find_redirect(&redirect.to_owned()) {
+        Some(r) => {
+            let _ = stream.write_all(
+                format!(
+                    "HTTP/1.1 301 Moved Permanently\r\n\
+                                      Location: {}\r\n\
+                                      \n",
+                    r
+                )
+                .as_bytes(),
+            );
+        }
+        None => Err("no redirect specified")?,
+    };
+
+    Ok(())
 }
