ce9b75de62ad7f029f98fce3eb1ed2e21cee2adb
Leonard-Orlando Menzel <79226837+lomenzel@users.noreply.github.com>
2024-05-09 14:08:57 +0200

    Add Nix support (#11)
    * add Nix support
    
    * add module. i know it wil fail but i need to test it
    
    * maybe use the right wsh :)
    
    * improve config options
    
    * remove nginx config because i dont get how to do it correctly
    
    * remove check for option host_mode because it doesnt work
    
    * changing random syntax hoping to get it right
    
    * ups it was the formatting :)
    
    * aaargh
    
    * aaargh
    
    * forgot mkOption
    
    * maybe string is the name?
    
    * lets try path
    
    * debug env variable
    
    * debug env variable
    
    * ich krieg die kriese
    
    * maybe it should be path
    
    * klammmern weg?
    
    * okay path ist es nicht
    
    * to string hmmm
    
    * update readme
    
    * Update README.md

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils"; nixpkgs.follows =
+    "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system; overlays = [cargo2nix.overlays.default];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0"; packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          wsh = (rustPkgs.workspace.wsh {}); default = packages.wsh;
+        }; nixosModules = {
+          default = ({ config, lib, pkgs, ... }:
+
+            with lib;
+
+            {
+              options = {
+                services.wsh = {
+                  enable = mkEnableOption "Enable the WSH service";
+                  port = mkOption {
+                    type = types.int; default = 8012; description =
+                    "Port for the WSH service";
+                  }; host_mode = mkOption {
+                    type = types.string; default = "mirror"; description =
+                    "Mode for the WSH service: 'mirror' or 'local'";
+                  }; mirror = {
+                    url = mkOption {
+                      type = types.string;
+                      default = "https://wsh.draculente.eu";
+                      description = "URL for the mirror";
+                    };
+                  }; configFile = mkOption {
+                    type = types.path;
+                    default = "/example/path/config.toml";
+                    description = "path for a configuration toml file";
+                  };
+                };
+              };
+
+              config = mkIf config.services.wsh.enable {
+                systemd.services.wsh = {
+                    description = "WSH Service";
+                    wantedBy= [ "multi-user.target" ];
+                    after = [ "network.target" ];
+                    serviceConfig = {
+                    ExecStart = "${packages.wsh}/bin/wsh";
+                    Environment = [
+                        "WEBCOMMAND_PORT=${toString config.services.wsh.port}"
+                        ''WEBCOMMAND_CONFIG=${
+                            if
+                                config.services.wsh.host_mode == "mirror"
+                            then
+                                config.services.wsh.mirror.url
+                            else
+                                config.services.wsh.configFile
+                        }''
+                        "WEBCOMMAND_HOST_MODE=${if config.services.wsh.host_mode == "local" then "true" else "false"}"
+                      ];
+                    };
+                };
+              };
+
+            }
+          );
+        };
+      }
+    );
+}
